a2c0319b2a97e671ab3e340e6db08e61c3ec0db4
Benjamin DosSantos <BlackPenSoftware@Gmail.com>
2016-03-17 16:48:33 -0400

    First push of old projects to GitHub

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld {
+
+	/** 
+	 * 
+	 *	Name: Benjamin DosSantos 
+	 *	Assignment: Hello World Application 
+	 *	Project Description: This file Prints the string "Hello World" to
+	 *   the system console.
+	 * 
+	 **/
+	
+	/** All the code that is to be written MUST go between the { Opening and Closing Brackets } Of the main function **/
+	
+	public static void main(String[] args) {
+		
+		
+		
+		/** Prints "Hello World" One Time On A New Line **/
+		
+		System.out.println("Hello World");		/** Anything between the quotations is printed to console **/	
+		
+	}/** End Of Main Meathod **/
+}/** End Of Hello World Class **/
